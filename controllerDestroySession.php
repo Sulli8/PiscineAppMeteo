111f85ebf3946dbc2cceaf4f9fde79da88ebdb85
Jason <ambajason2@gmail.com>
2021-10-13 11:40:15 +0200

    reset SessionId in 0

--- controllerDestroySession.php
+++ controllerDestroySession.php
@@ -1,5 +1,12 @@
-<?php 
+<?php
+require_once("manager.php"); 
 session_start();
+$manager = new manager();
+$pdo = $manager->connexion_bd();
+        $requestRegistration = $pdo->prepare("UPDATE user SET SessionId = 0 WHERE SessionId = ?");
+        $requestRegistration->execute(array($_SESSION['id']));
 session_destroy();
+
+
 header("Location:index.php");
 ?>
